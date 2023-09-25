3a81ed6e6b02a952ec59b84cf4c1c30328ccc184
Shadowist <shadowist@protonmail.com>
2023-09-24 18:38:13 -0700

    Update mx_utils.py
    Refactor logbook creation

--- source/mx_utils.py
+++ source/mx_utils.py
@@ -4,43 +4,21 @@ import sqlite3
 from . import sql_utils
 
 
-def create_plane_database(tail_number: str):
+def create_plane_database(tail_number: str) -> None:
     """ Creates the database for the plane. """
     conn: sqlite3.Connection = sql_utils.create_connection(f"databases/{tail_number}")
 
-    _create_engine_log_table(conn, tail_number)
-    _create_avionics_log_table(conn, tail_number)
-    _create_airframe_log_table(conn, tail_number)
+    _create_logbooks(conn)
 
 
-def _create_engine_log_table(conn: sqlite3.Connection, tail_number: str):
-    conn.execute("""
-        CREATE TABLE IF NOT EXISTS "Engine" (
-            "Date"	TEXT,
-            "Work Description"	TEXT,
-            "TTAF"	NUMERIC,
-            "Tach"	NUMERIC
-        );
-    """)
-
-
-def _create_avionics_log_table(conn: sqlite3.Connection, tail_number: str):
-    conn.execute("""
-        CREATE TABLE IF NOT EXISTS "Avionics" (
-            "Date"	TEXT,
-            "Work Description"	TEXT,
-            "TTAF"	NUMERIC,
-            "Tach"	NUMERIC
-        );
-    """)
-
-
-def _create_airframe_log_table(conn: sqlite3.Connection, tail_number: str):
-    conn.execute("""
-        CREATE TABLE IF NOT EXISTS "Airframe" (
-            "Date"	TEXT,
-            "Work Description"	TEXT,
-            "TTAF"	NUMERIC,
-            "Tach"	NUMERIC
-        );
-    """)
+def _create_logbooks(conn: sqlite3.Connection) -> None:
+    logbooks = ["Airframe", "Avionics", "Engine"]
+    for logbook in logbooks:
+        conn.execute(f"""
+            CREATE TABLE IF NOT EXISTS {logbook} (
+                "Date"	TEXT,
+                "Work Description"	TEXT,
+                "TTAF"	NUMERIC,
+                "Tach"	NUMERIC
+            );
+        """)
