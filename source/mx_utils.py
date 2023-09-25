c7f6ee187de1cd94132c8e88b8bda760b40dd373
Shadowist <shadowist@protonmail.com>
2023-09-24 18:42:31 -0700

    Update mx_utils.py
    Added Squawks

--- source/mx_utils.py
+++ source/mx_utils.py
@@ -8,17 +8,27 @@ def create_plane_database(tail_number: str) -> None:
     """ Creates the database for the plane. """
     conn: sqlite3.Connection = sql_utils.create_connection(f"databases/{tail_number}")
 
-    _create_logbooks(conn)
+    _create_tables(conn)
 
 
-def _create_logbooks(conn: sqlite3.Connection) -> None:
+def _create_tables(conn: sqlite3.Connection) -> None:
     logbooks = ["Airframe", "Avionics", "Engine"]
     for logbook in logbooks:
         conn.execute(f"""
             CREATE TABLE IF NOT EXISTS {logbook} (
-                "Date"	TEXT,
+                "Date"              TEXT,
                 "Work Description"	TEXT,
-                "TTAF"	NUMERIC,
-                "Tach"	NUMERIC
+                "TTAF"              NUMERIC,
+                "Tach"              NUMERIC
             );
         """)
+
+    conn.execute("""
+        CREATE TABLE IF NOT EXISTS Squawks (
+            "Date"          TEXT,
+            "Title"         TEXT,
+            "Description"   TEXT,
+            "Priority"      INTEGER,
+            "Status"        INTEGER
+        );
+    """)
