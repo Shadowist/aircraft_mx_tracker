ffd6d387e97792212c5806882f98a1b59b19bed5
Shadowist <shadowist@protonmail.com>
2023-09-24 20:15:43 -0700

    Update mx_utils.py
    Added function to pull mx entries from CSV.

--- source/mx_utils.py
+++ source/mx_utils.py
@@ -2,23 +2,34 @@
 
 import sqlite3
 from . import sql_utils
+from pathlib import Path
+import csv
+import os
+from datetime import datetime
 
 
-def create_plane_database(tail_number: str) -> None:
+def create_plane_database(tail_number: str, mx_csv: str = "", overwrite=False) -> None:
     """ Creates the database for the plane. """
+    if overwrite:
+        os.remove(f"databases/{tail_number}")
+
     conn: sqlite3.Connection = sql_utils.create_connection(f"databases/{tail_number}")
 
     _create_tables(conn)
 
+    if mx_csv:
+        _import_mx_from_csv(mx_csv, conn)
+
 
 def _create_tables(conn: sqlite3.Connection) -> None:
-    logbooks = ["Airframe", "Avionics", "Engine"]
+    logbooks = ["Airframe", "Avionics", "Engine", "Propeller"]
     for logbook in logbooks:
         conn.execute(f"""
             CREATE TABLE IF NOT EXISTS {logbook} (
+                "Tail Number"       TEXT,
                 "Logbook"           TEXT,
-                "Date"              TEXT,
-                "Work Description"	TEXT,
+                "Date of Service"   TEXT,
+                "Work Description"  TEXT,
                 "TTAF"              NUMERIC,
                 "Tach"              NUMERIC
             );
@@ -33,3 +44,44 @@ def _create_tables(conn: sqlite3.Connection) -> None:
             "Status"        INTEGER
         );
     """)
+
+
+def _import_mx_from_csv(filename: str, conn: sqlite3.Connection):
+    path = Path(filename)
+    if not path.exists():
+        raise Exception("Path does not exist!")
+
+    with open(filename, 'r') as csvfile:
+        reader = csv.DictReader(csvfile, delimiter=',')
+
+        for row in reader:
+            cur = conn.cursor()
+
+            desc = row['Work Description'].replace('"', '\'')
+            ttaf = -1
+            tach = -1
+            date = datetime.strptime(row['Date of Service'], '%b %d, %Y')
+
+            try:
+                ttaf = float(row['TTAF'])
+            except ValueError:
+                pass
+
+            try:
+                tach = float(row['TTAF'])
+            except ValueError:
+                pass
+
+            cmd = f"""
+INSERT INTO "{row['Logbook Type']}"
+VALUES(
+    "{row['Tail Number']}",
+    "{row['Logbook Type']}",
+    date("{date}"),
+    "{desc}",
+    {ttaf},
+    {tach}
+)
+"""
+            cur.execute(cmd)
+    conn.commit()
